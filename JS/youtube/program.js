cc1c0560d4ec39c95ae0aa678ba8279e72656052
Marina <marina@Alexs-MacBook-Pro.local>
2021-06-05 21:18:55 +0200

    update youtube project

--- JS/youtube/program.js
+++ JS/youtube/program.js
@@ -3,46 +3,89 @@ var input = document.querySelector('input');
 var button = document.querySelector('button');
 var main = document.querySelector('main');
 var preview = document.querySelector('#preview');
+var youtubeSearch = document.getElementById('youtubeSearch');
 
-
-function openVideo(videoId) {
-    preview.classList.add('visible');
-    preview.setAttribute('src', `https://www.youtube.com/embed/${videoId}`);
-
-}
-
-function createVideo(video) {
-    var vd = document.createElement('div');
-    var img = document.createElement('img');
-    var title = document.createElement('h5');
-    var desc = document.createElement('p');
-
-    img.setAttribute('src', video.snippet.thumbnails.default.url);
-    title.textContent = video.snippet.title;
-    desc.textContent = video.snippet.description;
-    vd.appendChild(img);
-    vd.appendChild(title);
-    vd.appendChild(desc)
-    vd.addEventListener("click", () => {
-        openVideo(video.id.videoId);
-        getInfo(video.id.videoId);
-    });
-    main.appendChild(vd);
-}
-
-function createVideos(videos) {
-    videos.forEach(video => createVideo(video))
-}
-
-function getInfo(id) {
-    var req = new XMLHttpRequest()
+function getInfo(videoId) {
+    var url;
+    var relatedUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${videoId}&type=video&key=${key}`;
     var defaultUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${input.value}&maxResults=15&key=${key}`;
-    var relatedUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${input.value}&maxResults=15&key=${key}`;
-    req.open('GET', id ? relatedUrl : defaultUrl)
+    
+    if (videoId instanceof MouseEvent){
+        removeYoutubeClip()
+        url = defaultUrl;
+    } else {
+        url = relatedUrl;
+    }
+
+    removeVideos();
+    var req = new XMLHttpRequest()
+    
+    req.open('GET', url)
     req.onload = function () {
-        createVideos(JSON.parse(req.responseText).items)
+        var videos = JSON.parse(req.responseText).items;
+        createVideos(videos);
     }
     req.send();
 }
 
+function createVideos(videos) {
+    videos.forEach(video => {
+        makeVideo(video)
+    });
+}
+
+function makeVideo(video) {
+    // pravimo elemente
+    var videoContainer = document.createElement('div'); // vraca instancu elementa (novog)
+    var videoImg = document.createElement('img');
+    var videoTitle = document.createElement('h5');
+    var videoDesc = document.createElement('p');
+
+    // setujemo video id
+
+    var videoId = video.id.videoId;
+
+    // setujemo event listener
+
+    videoContainer.addEventListener('click', () => {
+        openVideo(videoId);
+        getInfo(videoId)
+    });
+
+    // treba da popunim html elemente
+
+    if (video.snippet === undefined) {
+        return;
+    }
+
+    videoImg.src = video.snippet.thumbnails.medium.url;
+    videoTitle.textContent = video.snippet.title;
+    videoDesc.textContent = video.snippet.description;
+
+    // videoContainer je parent
+    videoContainer.appendChild(videoTitle);
+    videoContainer.appendChild(videoImg);
+    videoContainer.appendChild(videoDesc);
+
+    main.appendChild(videoContainer)
+}
+
+function openVideo(videoId) {
+    preview.classList.add("visible");
+    preview.src = `https://www.youtube.com/embed/${videoId}`;
+}
+
+function removeVideos() {
+    main.innerHTML = "";
+}
+
+function removeYoutubeClip() {
+    preview.classList.remove('visible')
+}
+
 button.addEventListener('click', getInfo)
+youtubeSearch.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+        button.click()
+    }
+})
