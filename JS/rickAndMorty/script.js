523b3e2d6ba905d3dd141d49d3203cdd3bd2fb4d
BIT Student <marinastanicic94@gmail.com>
2021-06-09 17:42:41 +0200

    rick&morty

---
+++ JS/rickAndMorty/script.js
@@ -0,0 +1,23 @@
+var main = document.querySelector('main');
+var container = document.querySelector('.container')
+
+import { getData } from './fetch.js';
+
+getData().then(data => function createPreview(data) {
+    
+    var preview = document.createElement("div");
+    preview.id = "preview";
+    var img = document.createElement("img");
+    img.src = data.image;
+    console.log(data);
+    var name = document.createElement("h4");
+    name.textContent = data.name;
+
+    preview.appendChild(img);
+    preview.appendChild(name);
+
+    container.appendChild(preview);
+    main.appendChild(container)
+
+   
+})
